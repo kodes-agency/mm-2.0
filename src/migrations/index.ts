8903966eff1accf9ee49c8245b3174f8803907d4
denevB <95876310+denevB@users.noreply.github.com>
2025-03-16 15:46:33 +0200

    origin

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20250316_133954 from './20250316_133954';
-
-export const migrations = [
-  {
-    up: migration_20250316_133954.up,
-    down: migration_20250316_133954.down,
-    name: '20250316_133954'
-  },
-];
